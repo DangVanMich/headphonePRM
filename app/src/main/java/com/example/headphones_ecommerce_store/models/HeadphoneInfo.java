a2747abbaaad3aa6d07f5852eb3afa8af8ad36cf
= <=cinepa.org@gmail.com>
2025-07-22 22:58:28 +0700

    feat(product): revert main home activity

--- app/src/main/java/com/example/headphones_ecommerce_store/models/HeadphoneInfo.java
+++ app/src/main/java/com/example/headphones_ecommerce_store/models/HeadphoneInfo.java
@@ -1,20 +1,17 @@
 package com.example.headphones_ecommerce_store.models;
-import java.io.Serializable;
 
-public class HeadphoneInfo implements Serializable {
-    private long id;
+public class HeadphoneInfo {
     private String name;
     private String brand;
     private double price;
     private String imageUrl;
 
-    public HeadphoneInfo(long id, String name, String brand, double price, String imageUrl) {
+    public HeadphoneInfo(String name, String brand, double price, String imageUrl) {
         this.name = name;
         this.brand = brand;
         this.price = price;
         this.imageUrl = imageUrl;
     }
-    public long getId() { return id; }
 
     public String getName() { return name; }
     public String getBrand() { return brand; }
